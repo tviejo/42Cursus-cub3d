a71c132e1c0681f6c36f4280e96723b4c1826f25
Thomas Viejo <tviejo@made-f0Br6s1.clusters.42paris.fr>
2024-08-16 18:05:20 +0200

    good libft include path

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tviejo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 16:29:44 by tviejo            #+#    #+#             */
-/*   Updated: 2024/08/16 16:30:23 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/08/16 18:04:40 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../libft/libft.h"
+# include "../libft/includes/libft.h"
 
 #endif
