cd6e68660ec60c343858e3eb03a4ccbc78155de2
Thomas Viejo <tviejo@made-f0Br6s1.clusters.42paris.fr>
2024-08-25 18:59:44 +0200

    added y and n for exit

--- srcs/exit_page/exit_page.c
+++ srcs/exit_page/exit_page.c
@@ -6,7 +6,7 @@
 /*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 17:22:34 by tviejo            #+#    #+#             */
-/*   Updated: 2024/08/25 18:32:47 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/08/25 18:55:52 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 int render_exit_page(t_cub3d *cub3d)
 {
-    mlx_string_put(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr, 1000, 500, 0xFF0000, "Exit Page");
+    mlx_string_put(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr, 950, 500, 0xFF0000, "Exit Page: y to exit, n to return");
     return (0);
 }
--- srcs/keys/keys.c
+++ srcs/keys/keys.c
@@ -6,48 +6,51 @@
 /*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 18:24:17 by tviejo            #+#    #+#             */
-/*   Updated: 2024/08/25 18:34:56 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/08/25 18:56:38 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../includes/cub3d.h"
+#include "../includes/cub3d.h"
 
-int		key_press(int keycode, t_cub3d *cub3d)
+int	key_press(int keycode, t_cub3d *cub3d)
 {
-    if (keycode == XK_Escape && cub3d->game.page == EXIT_PAGE)
-        ft_close(cub3d);
-    if (keycode == XK_Escape)
-        cub3d->game.page = EXIT_PAGE;
-    if (keycode == XK_w)
-        cub3d->keys.up = true;
-    if (keycode == XK_s)
-        cub3d->keys.down = true;
-    if (keycode == XK_a)
-        cub3d->keys.left = true;
-    if (keycode == XK_d)
-        cub3d->keys.right = true;
-    if (keycode == XK_Left)
-        cub3d->keys.turn_left = true;
-    if (keycode == XK_Right)
-        cub3d->keys.turn_right = true;
-    if (keycode == XK_space && cub3d->game.page == LANDING_PAGE)
-        cub3d->game.page = GAME_PAGE;
-    return (0);
+	if ((keycode == XK_Escape || keycode == XK_y)
+		&& cub3d->game.page == EXIT_PAGE)
+		ft_close(cub3d);
+	if (keycode == XK_Escape)
+		cub3d->game.page = EXIT_PAGE;
+	if (keycode == XK_w)
+		cub3d->keys.up = true;
+	if (keycode == XK_s)
+		cub3d->keys.down = true;
+	if (keycode == XK_a)
+		cub3d->keys.left = true;
+	if (keycode == XK_d)
+		cub3d->keys.right = true;
+	if (keycode == XK_Left)
+		cub3d->keys.turn_left = true;
+	if (keycode == XK_Right)
+		cub3d->keys.turn_right = true;
+	if (keycode == XK_space && cub3d->game.page == LANDING_PAGE)
+		cub3d->game.page = GAME_PAGE;
+	if (keycode == XK_n && cub3d->game.page == EXIT_PAGE)
+		cub3d->game.page = GAME_PAGE;
+	return (0);
 }
 
-int    key_release(int keycode, t_cub3d *cub3d)
+int	key_release(int keycode, t_cub3d *cub3d)
 {
-    if (keycode == XK_w)
-        cub3d->keys.up = false;
-    if (keycode == XK_s)
-        cub3d->keys.down = false;
-    if (keycode == XK_a)
-        cub3d->keys.left = false;
-    if (keycode == XK_d)
-        cub3d->keys.right = false;
-    if (keycode == XK_Left)
-        cub3d->keys.turn_left = false;
-    if (keycode == XK_Right)
-        cub3d->keys.turn_right = false;
-    return (0);
+	if (keycode == XK_w)
+		cub3d->keys.up = false;
+	if (keycode == XK_s)
+		cub3d->keys.down = false;
+	if (keycode == XK_a)
+		cub3d->keys.left = false;
+	if (keycode == XK_d)
+		cub3d->keys.right = false;
+	if (keycode == XK_Left)
+		cub3d->keys.turn_left = false;
+	if (keycode == XK_Right)
+		cub3d->keys.turn_right = false;
+	return (0);
 }
