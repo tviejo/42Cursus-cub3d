f6e0e544e348ef76cf760c11d4e901acf34051aa
tviejo <tviejo@student.42.fr>
2024-09-03 15:28:43 +0200

    multiple door fix

--- srcs/game_page/game_page.c
+++ srcs/game_page/game_page.c
@@ -6,7 +6,7 @@
 /*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 17:22:41 by tviejo            #+#    #+#             */
-/*   Updated: 2024/09/03 15:09:48 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/09/03 15:22:30 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	render_game_page(t_cub3d *cub3d)
 	update_player_pos(cub3d);
 	render_frame(cub3d);
 	draw_minimap(cub3d);
-	interact_door(cub3d, cub3d->player.pos);
 	//mlx_clear_window(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr);
 	mlx_put_image_to_window(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr,
 		cub3d->mlx.mlx_img.ptr, 0, 0);
+	interact_door(cub3d, cub3d->player.pos);
 	update_n_draw_fps(cub3d);
 	return (0);
 }
