711b9f938e2f915839620f330171b714397297e5
Thomas Viejo <tviejo@made-f0Br6s2.clusters.42paris.fr>
2024-09-05 13:44:17 +0200

    v1 mouse

--- srcs/game_page/game_page.c
+++ srcs/game_page/game_page.c
@@ -6,18 +6,35 @@
 /*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 17:22:41 by tviejo            #+#    #+#             */
-/*   Updated: 2024/09/03 15:22:30 by tviejo           ###   ########.fr       */
+/*   Updated: 2024/09/05 13:43:02 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	mouse(t_cub3d *cub3d)
+{
+	static int x = WINDOW_WIDTH / 2;
+	int	x1;
+	int	y1;
+
+	if (x < 100 || x > WINDOW_WIDTH - 100)
+	{
+		x = WINDOW_WIDTH / 2;
+		mlx_mouse_move(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr, x, WINDOW_HEIGHT / 2);
+	}
+	mlx_mouse_get_pos(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr, &x1, &y1);
+	rotate_player(&cub3d->player, (x1 - cub3d->inputs.mouse_x) * 0.001);
+	cub3d->inputs.mouse_x = x1;
+	cub3d->inputs.mouse_y = y1;
+}
+
 int	render_game_page(t_cub3d *cub3d)
 {
+	mouse(cub3d);
 	update_player_pos(cub3d);
 	render_frame(cub3d);
 	draw_minimap(cub3d);
-	//mlx_clear_window(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr);
 	mlx_put_image_to_window(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr,
 		cub3d->mlx.mlx_img.ptr, 0, 0);
 	interact_door(cub3d, cub3d->player.pos);
--- srcs/keys/mouse.c
+++ srcs/keys/mouse.c
@@ -3,11 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   mouse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ade-sarr <ade-sarr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 17:52:52 by tviejo            #+#    #+#             */
-/*   Updated: 2024/08/30 14:02:12 by ade-sarr         ###   ########.fr       */
+/*   Updated: 2024/09/05 13:27:31 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int    mouse_move(int button, int x, int y, t_cub3d *cub3d)
+{
+    (void)button;
+    cub3d->inputs.mouse_x = x;
+    cub3d->inputs.mouse_y = y;
+    return (EXIT_SUCCESS);
+}
