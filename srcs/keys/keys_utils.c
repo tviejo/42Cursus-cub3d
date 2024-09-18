e7a947618bbcb04e68de3594ae5869a8e323063d
Thomas Viejo <tviejo@made-f0Br3s5.clusters.42paris.fr>
2024-09-18 14:14:01 +0200

    better keys

--- srcs/keys/keys_utils.c
+++ srcs/keys/keys_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   keys_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ade-sarr <ade-sarr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tviejo <tviejo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 18:24:17 by tviejo            #+#    #+#             */
-/*   Updated: 2024/09/15 19:58:02 by ade-sarr         ###   ########.fr       */
+/*   Updated: 2024/09/18 14:02:04 by tviejo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	init_keys(t_cub3d *cub)
 	const int	height = cub->mlx.mlx_img.dim.height;
 
 	ft_memset(&cub->inputs, 0, sizeof(t_player_inputs));
-	cub->inputs.strafe_mode = true;
+	cub->inputs.strafe_mode = false;
 	mlx_mouse_move(cub->mlx.mlx_ptr, cub->mlx.win_ptr,
 		width / 2, height / 2);
 	cub->inputs.mouse_x = width / 2;
